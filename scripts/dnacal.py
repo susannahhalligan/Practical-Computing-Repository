145908f4df0f19c9ac68d87f2ab6afa34175082c
susannahhalligan <susannahhalligan@gmail.com>
2026-09-09 18:16:15 -0400

    DNACalc stuff

---
+++ scripts/dnacal.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+DNASeq = input("Enter DNA sequence: ")
+DNASeq = DNASeq.upper()
+DNASeq = DNASeq.replace(" ", "")
+
+print("\n"+DNASeq+"\n")
+
+SeqLength = len(DNASeq)
+
+print("Sequence Length: "+str(SeqLength))
+
+NumberA = DNASeq.count("A")
+
+NumberC = DNASeq.count("C")
+
+NumberG = DNASeq.count("G")
+
+NumberT = DNASeq.count("T")
+
+print("A: ", f'{NumberA/SeqLength:.2f}')
+print("C: ", f'{NumberC/SeqLength:.2f}')
+print("G: ", f'{NumberG/SeqLength:.2f}')
+print("T: ", f'{NumberT/SeqLength:.2f}')
+
+TotalStrong = NumberG + NumberC
+TotalWeak = NumberA + NumberT
+if SeqLength >= 14:
+	MeltTempLong = 64.9 + 41 * (TotalStrong - 16.4) / SeqLength
+	print("\nTm Long (>14): " + f'{MeltTempLong:.4f}' + " C\n"
+
+else:
+	MeltTempShort = (4 * TotalStrong) + (2 * TotalWeak)
+	print("\nTm Short (<14): " + f'{MeltTempShort:.4f}' + " C\n")
+
+

2a5e36faaf3c26370bb1c3d22e74fc3dcbd573a5
susannahhalligan <susannahhalligan@gmail.com>
2026-09-16 17:05:19 -0400

    getting ready for python/markdown

--- scripts/dnacal.py
+++ scripts/dnacal.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 
-DNASeq = input("Enter DNA sequence: ")
+import sys
+
+#DNASeq = input("Enter DNA sequence: ")
+DNASeq = sys.argv[1]
 DNASeq = DNASeq.upper()
 DNASeq = DNASeq.replace(" ", "")
 
@@ -27,10 +30,16 @@ TotalStrong = NumberG + NumberC
 TotalWeak = NumberA + NumberT
 if SeqLength >= 14:
 	MeltTempLong = 64.9 + 41 * (TotalStrong - 16.4) / SeqLength
-	print("\nTm Long (>14): " + f'{MeltTempLong:.4f}' + " C\n"
+	print("\nTm Long (>14): " + f'{MeltTempLong:.4f}' + " C\n")
 
 else:
 	MeltTempShort = (4 * TotalStrong) + (2 * TotalWeak)
 	print("\nTm Short (<14): " + f'{MeltTempShort:.4f}' + " C\n")
 
+BaseList = "ATCG"
+
+for Base in Baselist:
+	Percent = 100 * DNASeq.count(Base) / SeqLength
+	#print(Base+ " "+str(Percent))
+	print("%s: %4.1f" % (Base, Percent))
 
